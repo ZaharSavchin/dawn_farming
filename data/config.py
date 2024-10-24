1e989f907b6533ae37fc7fc3fda36eea039078dd
Tawer95 <73060710+Tawer95@users.noreply.github.com>
2024-10-24 18:11:36 +0500

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,6 @@
+API_KEY = '' # ChatGpt API_KEY
+MAX_RETRIES = 5 # Кол-во повторных попыток регистрации или логина
+RETRY_DELAY = 3 # Пауза между запросами в секундах
+REGISTER_ONLY = False # False = Login only | True = Register Only
+MAX_THREADS = 1  # Укажите количество потоков
+FARM_ONLY = False # Set True if you want only farm points
