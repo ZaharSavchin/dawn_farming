7a12f4f97925b4e3927fde4fe2089fb0d3a01b54
Barodun <56911178+Barodun@users.noreply.github.com>
2024-11-11 20:02:33 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
-API_KEY = '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' # ChatGpt API_KEY
+API_KEY = 'API_KEY' # ChatGpt API_KEY
 MAX_RETRIES = 5 # Кол-во повторных попыток регистрации или логина
 RETRY_DELAY = 3 # Пауза между запросами в секундах
 REGISTER = False # False = Login only | True = Register, then login
 MAX_THREADS = 10  # Укажите количество потоков
 FARM_ONLY = True # Set True if you want only farm points
-REF_CODE = 'nxvz54eh' # Referal code for registration
+REF_CODE = 'REF_CODE' # Referal code for registration
 BOOST_USERS = False # Set True if you want to boost users
 VERIFY_ONLY = False # True = Verify only | False = Register or farm
 
@@ -12,6 +12,6 @@ VERIFY_ONLY = False # True = Verify only | False = Register or farm
 # Google export configuration #
 ##########################################################################
 
-EXPORT_DATA = True # True if want to export | False if not
-SHEET_ID = '18ieXul0QrwYlXcbpe-V3lF9DZs4hyKsuXvUYcwzexPk' # Your Google Sheet ID
-LIST_NAME = 'Dawn' # Your Google Sheet List name
+EXPORT_DATA = False # True if want to export | False if not
+SHEET_ID = 'SHEET_ID' # Your Google Sheet ID
+LIST_NAME = 'LIST_NAME' # Your Google Sheet List name
